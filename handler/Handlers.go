f98cedab60fda5483895ecf21a2b3a4e1228ce03
Your Name <you@example.com>
2025-01-23 11:59:38 +0100

    edit the  bg styles . and seperate the files ...

---
+++ handler/Handlers.go
@@ -0,0 +1,145 @@
+package ascii
+
+import (
+	"net/http"
+	"os"
+	"strings"
+	"text/template"
+	"unicode"
+
+	ascii "ascii/functions"
+)
+
+var Tp *template.Template
+
+type ErrorPage struct {
+	Code         int
+	ErrorMessage string
+}
+
+func StyleFunc(w http.ResponseWriter, r *http.Request) {
+	filePath := strings.TrimPrefix(r.URL.Path, "/")
+	File, err := os.Stat(filePath)
+
+	if err != nil || File.IsDir() {
+
+		errore := ErrorPage{
+			Code:         http.StatusNotFound,
+			ErrorMessage: "The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.",
+		}
+
+		w.WriteHeader(http.StatusNotFound)
+		Tp.ExecuteTemplate(w, "notfound.html", errore)
+		return
+	}
+	http.StripPrefix("/styles", http.FileServer(http.Dir("styles"))).ServeHTTP(w, r)
+}
+
+func ResultFunc(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/ascii-art" {
+		errore := ErrorPage{
+			Code:         http.StatusNotFound,
+			ErrorMessage: "The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.",
+		}
+
+		w.WriteHeader(http.StatusNotFound)
+		Tp.ExecuteTemplate(w, "notfound.html", errore)
+		return
+	}
+
+	if r.Method != http.MethodPost {
+		errore := ErrorPage{
+			Code:         http.StatusMethodNotAllowed,
+			ErrorMessage: "The request method is not supported for the requested resource.",
+		}
+
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		Tp.ExecuteTemplate(w, "notfound.html", errore)
+		return
+	}
+
+	word := r.FormValue("word")
+	typee := r.FormValue("typee")
+
+	var errorMessage string
+
+	if word == "" {
+		errorMessage = "Please enter a word."
+	} else if typee == "" {
+		errorMessage = "Please select a type."
+	} else if len(word) >= 1000 {
+		errorMessage = "The word length should not exceed 1000 characters."
+	}
+
+	for i := 0; i < len(word); i++ {
+		if unicode.IsLetter(rune(word[i])) && (word[i] < 32 || word[i] > 126) {
+			errorMessage = "Invalid characters."
+			break
+		}
+	}
+
+	if errorMessage != "" {
+		w.WriteHeader(http.StatusBadRequest)
+		Tp.ExecuteTemplate(w, "index.html", errorMessage)
+		return
+	}
+
+	LastResult := ascii.Ascii(word, typee)
+
+	if LastResult == "" {
+		errore := ErrorPage{
+			Code:         http.StatusInternalServerError,
+			ErrorMessage: "Something went wrong on our end. Please try again later.",
+		}
+
+		w.WriteHeader(http.StatusInternalServerError)
+		Tp.ExecuteTemplate(w, "notfound.html", errore)
+		return
+	}
+
+	err := Tp.ExecuteTemplate(w, "result.html", LastResult)
+	if err != nil {
+		errore := ErrorPage{
+			Code:         http.StatusInternalServerError,
+			ErrorMessage: "Something went wrong on our end. Please try again later.",
+		}
+
+		w.WriteHeader(http.StatusInternalServerError)
+		Tp.ExecuteTemplate(w, "notfound.html", errore)
+	}
+}
+
+func FormFunc(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		errore := ErrorPage{
+			Code:         http.StatusNotFound,
+			ErrorMessage: "The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.",
+		}
+
+		w.WriteHeader(http.StatusNotFound)
+		Tp.ExecuteTemplate(w, "notfound.html", errore)
+		return
+	}
+
+	if r.Method != http.MethodGet {
+		errore := ErrorPage{
+			Code:         http.StatusMethodNotAllowed,
+			ErrorMessage: "The request method is not supported for the requested resource.",
+		}
+
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		Tp.ExecuteTemplate(w, "notfound.html", errore)
+		return
+	}
+
+	err := Tp.ExecuteTemplate(w, "index.html", nil)
+	if err != nil {
+		errore := ErrorPage{
+			Code:         http.StatusInternalServerError,
+			ErrorMessage: "Something went wrong on our end. Please try again later.",
+		}
+
+		w.WriteHeader(http.StatusInternalServerError)
+		Tp.ExecuteTemplate(w, "notfound.html", errore)
+	}
+}
--- main.go
+++ main.go
@@ -3,173 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
-	"strings"
 	"text/template"
-	"unicode"
 
-	ascii "ascii/functions"
+	handler "ascii/handler"
 )
-
-var tp *template.Template
-
-type ErrorPage struct {
-	Code         int
-	ErrorMessage string
-}
 
 func main() {
 	var err error
 
-	tp, err = template.ParseGlob("template/*.html")
+	handler.Tp, err = template.ParseGlob("template/*.html")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Error parsing templates: %v\n", err)
+		return
 	}
 
-	http.HandleFunc("/styles/", styleFunc)
-	http.HandleFunc("/ascii-art", ResultFunc)
-	http.HandleFunc("/", formFunc)
+	// Register handlers
+	http.HandleFunc("/styles/", handler.StyleFunc)
+	http.HandleFunc("/ascii-art", handler.ResultFunc)
+	http.HandleFunc("/", handler.FormFunc)
+
 	fmt.Println("Server running at http://localhost:8080/")
 	http.ListenAndServe(":8080", nil)
-}
-
-func styleFunc(w http.ResponseWriter, r *http.Request) {
-	filePath := "styles" + strings.TrimPrefix(r.URL.Path, "/styles")
-
-	if _, err := os.Stat(filePath); os.IsNotExist(err) || r.URL.Path == "/styles/" || !strings.HasSuffix(r.URL.Path, "css") {
-
-		errore := ErrorPage{
-			Code:         http.StatusNotFound,
-			ErrorMessage: "The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.",
-		}
-
-		w.WriteHeader(http.StatusNotFound)
-		tp.ExecuteTemplate(w, "notfound.html", errore)
-		return
-	}
-	http.StripPrefix("/styles", http.FileServer(http.Dir("styles"))).ServeHTTP(w, r)
-}
-
-func formFunc(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		errore := ErrorPage{
-			Code:         http.StatusNotFound,
-			ErrorMessage: "The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.",
-		}
-
-		w.WriteHeader(http.StatusNotFound)
-
-		tp.ExecuteTemplate(w, "notfound.html", errore)
-		return
-	}
-
-	if r.Method != http.MethodGet {
-
-		errore := ErrorPage{
-			Code:         http.StatusMethodNotAllowed,
-			ErrorMessage: "  The request method is not supported for the requested resource. Please use the correct HTTP method. ",
-		}
-
-		w.WriteHeader(http.StatusMethodNotAllowed)
-
-		tp.ExecuteTemplate(w, "notfound.html", errore)
-		return
-	}
-
-	err := tp.ExecuteTemplate(w, "index.html", nil)
-	if err != nil {
-
-		errore := ErrorPage{
-			Code:         http.StatusInternalServerError,
-			ErrorMessage: " Something went wrong on our end. We are working to resolve the issue. Please try again later.  ",
-		}
-
-		w.WriteHeader(http.StatusInternalServerError)
-
-		tp.ExecuteTemplate(w, "notfound.html", errore)
-		return
-	}
-}
-
-func ResultFunc(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/ascii-art" {
-
-		errore := ErrorPage{
-			Code:         http.StatusNotFound,
-			ErrorMessage: "The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.",
-		}
-
-		w.WriteHeader(http.StatusNotFound)
-		tp.ExecuteTemplate(w, "notfound.html", errore)
-		return
-	}
-
-	if r.Method != http.MethodPost {
-
-		errore := ErrorPage{
-			Code:         http.StatusMethodNotAllowed,
-			ErrorMessage: " The request method is not supported for the requested resource. Please use the correct HTTP method.",
-		}
-
-		w.WriteHeader(http.StatusMethodNotAllowed)
-
-		tp.ExecuteTemplate(w, "notfound.html", errore)
-		return
-	}
-
-	word := r.FormValue("word")
-	typee := r.FormValue("typee")
-
-	var errorMessage string
-
-	if word == "" {
-		errorMessage = "Please enter a word."
-	} else if typee == "" {
-		errorMessage = "Please select a type."
-	} else if len(word) >= 1000 {
-		errorMessage = "The word length should not exceed 1000 characters."
-	}
-
-	for i := 0; i < len(word); i++ {
-		if unicode.IsLetter(rune(word[i])) && (word[i] < 32 || word[i] > 126) {
-			errorMessage = "invalid charts"
-			break
-		}
-	}
-
-	if errorMessage != "" {
-
-		w.WriteHeader(http.StatusBadRequest)
-		tp.ExecuteTemplate(w, "index.html", errorMessage)
-
-		return
-	}
-
-	LastResult := ascii.Ascii(word, typee)
-
-	if LastResult == "" {
-
-		errore := ErrorPage{
-			Code:         http.StatusInternalServerError,
-			ErrorMessage: " Something went wrong on our end. We are working to resolve the issue. Please try again later.  ",
-		}
-
-		w.WriteHeader(http.StatusInternalServerError)
-
-		tp.ExecuteTemplate(w, "notfound.html", errore)
-		return
-	}
-
-	err := tp.ExecuteTemplate(w, "result.html", LastResult)
-	if err != nil {
-		errore := ErrorPage{
-			Code:         http.StatusInternalServerError,
-			ErrorMessage: " Something went wrong on our end. We are working to resolve the issue. Please try again later.  ",
-		}
-
-		w.WriteHeader(http.StatusInternalServerError)
-
-		tp.ExecuteTemplate(w, "notfound.html", errore)
-		return
-	}
 }
